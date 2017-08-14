a3278c00347a4eca43fbbbda50281a61afe0ba0f
Pravin Tawade <30718551+pravin691983@users.noreply.github.com>
2017-08-14 12:47:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'First article | Pravin Tawade',
+    heading : 'First article',
+    date : '4th Aug 2017',
+    content : `
+        <p>
+            This is first page content ...!!!
+        </p>
+        `
+};
+
+function createHtmlTemplate(data) {
+    
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlContent = `
+    <html>
+    	<head>
+    		<title>${title}</title>
+    	</head>
+    	<body>
+    		<div class='Container'>
+    		    <div>
+    		        <a href="/">Home</a>
+                </div>
+                <hr/>
+            
+    	        <div>
+    			    <h1>${heading}</h1>
+                </div>
+                
+                <div>
+    			    <p>${date}</p>
+                </div>
+                
+                <div>
+    			    <p>${content}</p>
+                </div>
+                
+            </div>
+            
+    	</body>
+    </html>
+    `
+    ;
+    
+    return htmlContent;
+}
+
+app.get('/article-one', function(req, res) {
+    res.send(createHtmlTemplate(articleOne))
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
